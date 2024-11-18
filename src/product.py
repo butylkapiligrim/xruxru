7fe04183cab01642ce1653dc1c61249a45080278
butylkapiligrim <butylkapiligrim@gmail.com>
2024-11-18 15:18:19 +0300

    Update product.py

--- src/product.py
+++ src/product.py
@@ -1,37 +1,37 @@
-from typing import Any
-
-
 class Product:
     name: str
     description: str
     __price: float
     quantity: int
 
-    def __init__(self, name: Any, description: Any, price: Any, quantity: Any) -> None:
+    def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
         self.__price = price
         self.quantity = quantity
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
-    def __add__(self, other: Any) -> Any:
-        return self.price + self.quantity + other.price * other.quantity
+    def __add__(self, other):
+        if type(self) is type(other):
+            return self.price * self.quantity + other.price * other.quantity
+        else:
+            raise TypeError("Неа")
 
     @property
-    def price(self) -> Any:
+    def price(self):
         return self.__price
 
     @price.setter
-    def price(self, price: Any) -> None:
+    def price(self, price):
         if price > 0:
             self.__price = price
         else:
             print("Цена не должна быть нулевая или отрицательная")
 
     @classmethod
-    def new_product(cls, parameters: Any) -> Any:
+    def new_product(cls, parameters):
         n_product = cls(
             parameters["name"],
             parameters["description"],
